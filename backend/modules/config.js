35e4e88b38ab1d62eaebf0ed01ee71371251ca1c
zickbalazs <zickbalazs@turr.hu>
2023-04-20 14:07:59 +0200

    sql

--- backend/modules/config.js
+++ backend/modules/config.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
-
+let sql = require('mysql2');
 
 module.exports = {
     port: process.env.PORT,
-    db:{
+    dbconnection : sql.createPool({
+        database:process.env.DBNAME,
         host: process.env.DBHOST,
-        name: process.env.DBNAME,
         user: process.env.DBUSER,
-        pass: process.env.DBPASS
-    },
+        password: process.env.DBPASS,
+        connectionLimit: process.env.DBLIMIT
+    }) ,
     jwtsecret: process.env.TOKENSECRET
 }

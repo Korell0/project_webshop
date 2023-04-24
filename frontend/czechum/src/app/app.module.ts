811c22597e5845c2cacfe78e3c5dbbf10b22f0e9
zamboilles21 <zamboilles@turr.hu>
2023-04-24 08:36:22 +0200

    homepage

--- frontend/czechum/src/app/app.module.ts
+++ frontend/czechum/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoComponent } from './components/info/info.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoComponent
+    InfoComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,

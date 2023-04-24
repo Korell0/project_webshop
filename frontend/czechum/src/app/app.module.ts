38540422a214e3d06b6523c4a1773bfb12202d10
vighakos <vighakos@turr.hu>
2023-04-24 11:15:04 +0200

    NAVBAR ÉS INFORMÁCIÓS OLDAL

--- frontend/czechum/src/app/app.module.ts
+++ frontend/czechum/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoComponent } from './components/info/info.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfoComponent
+    InfoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

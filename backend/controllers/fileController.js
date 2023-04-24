681524f8b21cc6f9aacd354ed7207127a6080e56
DavidKocsis0204 <kocsisdavid20020204@gmail.com>
2023-04-24 08:44:28 +0200

    file feltöltés

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -1,6 +1,19 @@
-let Router = require('express').Router();
+const express = require('express');
+const multer  = require('multer');
+const upload = multer({ dest: './uploads/' });
+const config = require('../modules/config');
+const mysql = require('mysql');
+var connection = mysql.createConnection(config);
 
+const app = express();
 
-
+app.post('/item', upload.single('image'), (req, res)=>{
+    let image = req.file;
+    let text = req.body; 
+    connection.query(`INSERT INTO \`**dbname**\` (\`**fields**\`) VALUES (\`**values**\`)`, (err, result)=>{
+        if(err) res.status(500).send(err);
+        else res.status(200).send(result);
+    })
+})
 
 module.exports = Router;

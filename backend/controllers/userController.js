e9d168526df58e082607c5d869313f0205c62672
zickbalazs <zickbalazs@turr.hu>
2023-04-24 12:18:45 +0200

    Tokencheck

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,29 @@
+let jose = require('jose');
 let sha256 = require('crypto-js/sha256');
 let db = require('../modules/config').dbconnection;
+let jwtsecret = new TextEncoder().encode(require('../modules/config').jwtsecret);
 let Router = require('express').Router();
+let token = require('../modules/tokens').Check;
+Router.post('/login', (req,res)=>{
+    
+})
 
 
-Router.
-
-
-
+Router.post('/tokencheck', token() ,(req,res)=>{
+    res.status(200).send({message: 'success'});
+})
+Router.get('/testToken', async (req,res)=>{
+    res.status(200).send(
+        await new jose.SignJWT({
+            admin:true,
+            id:1,
+        })
+          .setProtectedHeader({alg: 'HS256'})
+          .setIssuedAt()
+          .setIssuer('Czechum')
+          .setAudience('Czechum:User')
+          .setExpirationTime('1d')
+          .sign(jwtsecret)
+    );
+})
 module.exports = Router;

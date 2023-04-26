700879e216596416a4d1bdeae7ddf57a4c7a96b9
SzaboPeter20 <2001szabopeter@gmail.com>
2023-04-26 14:12:54 +0200

    punching women and minorities

--- frontend/czechum/src/app/app-routing.module.ts
+++ frontend/czechum/src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InfoComponent } from './components/info/info.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import {KosarComponent} from'./components/kosar/kosar.component';
+import {OrderComponent} from'./components/order/order.component';
+import {ProudctComponent} from'./components/proudct/proudct.component';
+import {LoginComponent} from'./components/login/login.component';
 
 const routes: Routes = [
   { path: 'info', component: InfoComponent },
-  { path: 'homepage', component: HomepageComponent }
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'kosar', component: KosarComponent },
+  { path: 'rendelesek', component: OrderComponent },
+  { path: 'termekek', component: ProudctComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'reg', component: HomepageComponent }
 ];
 
 @NgModule({
